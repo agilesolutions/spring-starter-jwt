c2adc0813361a6962f3ca461b64e5027d03b7c35
Robert Rong <robert.rong@agile-solutions.ch>
2020-03-14 13:50:22 +0100

    f

--- src/main/java/com/agilesolutions/security/template/JWTRestTemplate.java
+++ src/main/java/com/agilesolutions/security/template/JWTRestTemplate.java
@@ -3,20 +3,14 @@ package com.agilesolutions.security.template;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
-import com.agilesolutions.security.util.JwtTokenUtil;
-
 @Configuration
 public class JWTRestTemplate {
-
-	@Autowired
-	private JwtTokenUtil jwtTokenUtil;
 
 	@Bean
 	public RestTemplate restTemplate() {
